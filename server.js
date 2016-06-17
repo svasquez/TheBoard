849251e538cb2af3aa72b7b11af1e9602104dd91
Smill Vásquez <smillalexander@gmail.com>
2016-06-17 19:43:24 -0400

    Example with ejs view engine

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var http = require("http");
 var express = require("express");
 var app = express();
+var ejsEngine = require("ejs-locals");
 
 //Setup the view engine
-app.set("view engine","jade");
+//app.set("view engine","jade");
+app.engine("ejs",ejsEngine); // supports master page
+app.set("view engine","ejs"); // ejs view engine
 
 app.get("/", (req, res) => {
     //res.send("<html><body><h1>Express</h1></body></html>");
-    res.render("jade/index", { title: "Express with Jade"});
+    res.render("ejs/index", { title: "Express with allalalalalallalalalalal"});
 });
 
 var server = http.createServer(app);
