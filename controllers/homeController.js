7e5e2f5a6c7180d44c6aff9076def3f843a2f86e
Smill Vásquez <smillalexander@gmail.com>
2016-06-19 23:04:16 -0400

    Using bower package

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,17 @@
 (function (homeController) {
+    var data = require("../data");
     homeController.init = function (app) {
         app.get("/", (req, res) => {
-    //res.send("<html><body><h1>Express</h1></body></html>");
-           res.render("index", { title: "Express with Vash" , description : "This is from Server!!"});
+        data.getNoteCategories(function (err,results) {
+            
+        
+        res.render("index", { 
+            title: "Express with Vash" , 
+            description : "This is from Server!!", 
+            error:err,
+            categories : results
+        });
     });
+});
 }
 })(module.exports);
--- server.js
+++ server.js
@@ -6,7 +6,10 @@ var controllers = require("./controllers");
 //Setup the view engine
 //app.set("view engine","jade");
 //app.engine("ejs",ejsEngine); // supports master page
-app.set("view engine","vash"); // ejs view engine
+app.set("view engine","vash"); // vash view engine
+
+//set the public static resource folder
+app.use(express.static(__dirname + "/public"));
 
 controllers.init(app);
 var server = http.createServer(app);
