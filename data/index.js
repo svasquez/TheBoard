2e2735fe058fcf05b8144a00c9f0998d452231b6
Smill Vásquez <smillalexander@gmail.com>
2016-07-01 10:57:15 -0400

    Seeding database using mongoose driver

--- data/index.js
+++ data/index.js
@@ -1,7 +1,40 @@
 (function (data) {
     var seedData = require("./seedData");
-    
+    var database = require("./database");
     data.getNoteCategories = function (next) {
         next(null, seedData.initialNotes);
     };
+
+    function seedDatabase() {
+        database.getDb(function (err,db){
+            if(err){
+                console.log("Failed to seed database.");
+
+            } else {
+                //Test to see data exist
+                 db.notes.count(function (err,count) {
+                     if(err){
+                        console.log("Failed to seed database. " + err);
+                     } else {
+                         if(count == 0){
+                             console.log("Seeding database...");
+                             seedData.initialNotes.forEach(function (item) {
+                                 db.notes.insert(item,function (error) {
+                                     if(error) console.log("Failed in insert item.");
+                                 })
+                             });
+                             console.log("Data inserted.")
+                         } else {
+                             console.log("Database already seed");
+                         }
+                     }
+                 });                   
+            }
+
+            //console.log("");
+
+        });
+    }
+
+    seedDatabase();
 })(module.exports);
