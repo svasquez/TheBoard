38b79f67e079ecd3e78f268866ef55134512ce9f
Smill Vásquez <smillalexander@gmail.com>
2016-07-23 00:55:27 -0400

    Notes controller was create, was used http angular functionality to get api notes

--- public/js/notesView.js
+++ public/js/notesView.js
@@ -1,3 +1,18 @@
 (function(angular) {
     var theModule = angular.module("notesView",[]);
+    theModule.controller("notesViewController",
+    ["$scope","$window","$http",
+     function ($scope,$window,$http) {
+         $scope.notes = []
+        var urlParts = $window.location.pathname.split("/");
+        var categoryName = urlParts[urlParts.length - 1];
+        var notesUrl = "/api/notes/" + categoryName;
+        $http.get(notesUrl)
+        .then(function (result) {
+            //Success
+            $scope.notes = result.data;
+        }, function (error) {
+            alert(error);
+        })
+    }]);
 })(window.angular);
